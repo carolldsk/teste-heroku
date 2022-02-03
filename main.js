5e5368a034a134377463db67f0d1b88c85135b5b
Carolina Vieira <carolldsk@gmail.com>
2022-02-03 00:05:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const app = express();
   
 
 //landing page
-app.get('/',function(req,res) {
-    app.use(express.static(__dirname));
-    res.sendFile(__dirname + "/index.html");
+app.use('/', (req, res, next) => {
+    let welcome = "oieee :D";
+    res.status(200).json({ welcome });
 });
+
 
 //listem port
 app.listen(3000);

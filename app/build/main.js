46a57312ece197c06d8c931e4156bfa2bee4ff4d
Ana Carolina Vieira Batista <ana.batista@estadao.com>
2022-02-03 09:24:54 -0300

    layout improving

--- app/build/main.js
+++ app/build/main.js
@@ -1,17 +1,21 @@
-const dotenv = require('dotenv');
+const dotenv  = require('dotenv');
+const  path   = require('path');
 const express = require('express');
-const cors = require("cors");
-dotenv.config({ path: './.env' });
+const cors    = require("cors");
+const app     = express();
 
-const app = express();
+dotenv.config({ path: './.env' });
 app.use(cors());
-  
+
 
 //landing page
-app.use('/', (req, res, next) => {
-    let welcome = "oieee :D";
-    res.status(200).json({ welcome });
+app.get('/',function(req,res) {
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    console.log(path.join(__dirname, '..', 'public/resources'));
+    res.sendFile(path.join(__dirname, '..','public/index.html'));
 });
+
 
 
 //listem port
